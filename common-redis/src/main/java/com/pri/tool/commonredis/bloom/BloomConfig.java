7275e890349b40cde655c663245cf54b11274fda
wangsong <song1.wang@symbio.com>
2020-12-09 11:42:44 +0800

    first commit

---
+++ common-redis/src/main/java/com/pri/tool/commonredis/bloom/BloomConfig.java
@@ -0,0 +1,20 @@
+package com.pri.tool.commonredis.bloom;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.Funnel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wang.song
+ * @date 2020-12-09 10:05
+ * @Desc
+ */
+@Configuration
+public class BloomConfig {
+    @Bean
+    public BloomFilterHelper<String> initBloomFilterHelper() {
+        return new BloomFilterHelper<>((Funnel<String>) (from, into) -> into.putString(from, Charsets.UTF_8).putString(from, Charsets.UTF_8), 1000000, 0.01);
+    }
+
+}
---
+++ common-redis/src/main/java/com/pri/tool/commonredis/bloom/RedisBloomUtil.java
@@ -0,0 +1,42 @@
+package com.pri.tool.commonredis.bloom;
+
+import com.google.common.base.Preconditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wang.song
+ * @date 2020-12-09 10:06
+ * @Desc 布隆过滤器util
+ */
+@Component
+public class RedisBloomUtil {
+    @Autowired
+    private RedisTemplate redisTemplate;
+    /**
+     * 根据给定的布隆过滤器添加值
+     */
+    public <T> void addByBloomFilter(BloomFilterHelper<T> bloomFilterHelper, String key, T value) {
+        Preconditions.checkArgument(bloomFilterHelper != null, "bloomFilterHelper不能为空");
+        int[] offset = bloomFilterHelper.murmurHashOffset(value);
+        for (int i : offset) {
+            redisTemplate.opsForValue().setBit(key, i, true);
+        }
+    }
+
+    /**
+     * 根据给定的布隆过滤器判断值是否存在
+     */
+    public <T> boolean includeByBloomFilter(BloomFilterHelper<T> bloomFilterHelper, String key, T value) {
+        Preconditions.checkArgument(bloomFilterHelper != null, "bloomFilterHelper不能为空");
+        int[] offset = bloomFilterHelper.murmurHashOffset(value);
+        for (int i : offset) {
+            if (!redisTemplate.opsForValue().getBit(key, i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
